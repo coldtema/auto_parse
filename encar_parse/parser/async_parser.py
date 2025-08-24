49548bf6e89058f7fde474caa1a97bbdedc78593
coldtema <coldtema1703@gmail.com>
2025-08-24 14:16:48 +0300

    finished async parser for car

--- encar_parse/parser/async_parser.py
+++ encar_parse/parser/async_parser.py
@@ -26,6 +26,7 @@ class AsyncCarParser():
                     "Accept": "application/json, text/plain, */*",
         }
         self.batch = []
+        self.results = []
 
     def run(self):
         self.get_cookies()
@@ -39,7 +40,7 @@ class AsyncCarParser():
         for i in range(math.ceil(self.car_count/self.batch_size)):
             self.batch = Car.objects.filter(encar_id__in=self.encar_ids[i*self.batch_size:(i+1)*self.batch_size], manufacturer=None, model=None, version=None, version_details=None)
             self.go_through_batch()
-            # self.save_to_db()
+            self.save_to_db()
 
 
     def go_through_batch(self):
@@ -47,22 +48,24 @@ class AsyncCarParser():
         for car in self.batch:
             list_api_urls.append(f'{self.encar_api_url}{car.encar_id}')
         print('Запуск')
-        results = asyncio.run(self.get_info(list_api_urls))
-        print(results)
+        self.results = asyncio.run(self.get_info(list_api_urls))
+        
     
 
     async def fetch(self, session, url):
         async with session.get(url, timeout=10) as response:
             response = await response.json()
+            photos_list = response['photos']
             detail_dict = {
-                'encar_id': response['vehicleId'], 
+                'encar_id': url.split('/')[-1], 
                 'manufacturer': response['category']['manufacturerEnglishName'],
                 'model': response['category']['modelGroupEnglishName'],
                 'version': response['category']['gradeEnglishName'],
                 'version_details': response['category']['gradeDetailEnglishName'],
                 'options': response['options']['standard'],
                 'color': response['spec']['colorName'],
-                'engine_capacity': response['spec']['displacement']
+                'engine_capacity': response['spec']['displacement'],
+                'number_of_photos': max(map(lambda x: int(x['code']), photos_list))
             }
             return detail_dict
 
@@ -79,8 +82,20 @@ class AsyncCarParser():
 
 
     def save_to_db(self):
-        Car.objects.bulk_update(fields=['manufacturer', 'model', 'version', 'version_details'], objs=self.batch)
-        self.new_elems = []
+        self.updated_batch = []
+        for result in self.results:
+            car_to_update = self.batch.get(encar_id=result['encar_id'])
+            car_to_update.manufacturer = result['manufacturer']
+            car_to_update.model = result['model']
+            car_to_update.version = result['version']
+            car_to_update.version_details = result['version_details']
+            car_to_update.options = result['options']
+            car_to_update.color = result['color']
+            car_to_update.engine_capacity = result['engine_capacity']
+            car_to_update.number_of_photos = result['number_of_photos']
+            self.updated_batch.append(car_to_update)
+        Car.objects.bulk_update(fields=['manufacturer', 'model', 'version', 'version_details', 'engine_capacity', 'color', 'options', 'number_of_photos'], objs=self.updated_batch)
+        self.results = []
 
 
 
