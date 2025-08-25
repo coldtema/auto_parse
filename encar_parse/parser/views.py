519b54bd501129b537a8ac9442e80c064c986048
coldtema <coldtema1703@gmail.com>
2025-08-25 17:01:14 +0300

    added viewfunc for showing vechile

--- encar_parse/parser/views.py
+++ encar_parse/parser/views.py
@@ -2,6 +2,8 @@ from django.shortcuts import render
 from django.http import HttpResponse
 from parser.raw_parser import CarParser, TruckParser
 from parser.async_parser import AsyncCarParser, AsyncTruckParser
+from .forms import CarArtikulForm
+from .models import Car, Truck, CarOption, TruckOption, OptionCategory
 
 
 
@@ -31,6 +33,8 @@ def async_car(request):
     c_p.run()
     del c_p
     return HttpResponse('oks')
+
+
 def index(request):
     if request.method == 'POST':
         form = CarArtikulForm(request.POST)
@@ -45,3 +49,25 @@ def index(request):
     else:
         form = CarArtikulForm()
     return render(request, 'parser/index.html', {'form': form})
+
+
+def vechile(request):
+    artikul = request.GET.get('artikul')
+    kind = request.GET.get('kind')
+    if artikul.isdigit():
+        if kind == 'truck':
+            truck = Truck.objects.get(encar_id=artikul)
+            all_truck_options = OptionCategory.objects.filter(vechile='TRUCK').prefetch_related('truckoption_set')
+            current_truck_options = list(map(lambda x: TruckOption.objects.get(encar_id=x).id, eval(truck.options)))
+            return render(request, 'parser/vechile.html', context={'all_options_list': all_truck_options,
+                                                                   'current_options_list': current_truck_options,
+                                                                   'full_name': f'{truck.manufacturer} {truck.model} {truck.version}',
+                                                                   'number_of_photos': truck.number_of_photos,
+                                                                   'photo_url': truck.photo_url,
+                                                                   'encar_id': truck.encar_id,
+                                                                   'category': truck.version_details
+                                                                   })
+        if kind == 'car':
+            car = Car.objects.get(encar_id=artikul)
+        return render(request, 'parser/vechile.html')
+    return render(request, 'parser/vechile.html')
