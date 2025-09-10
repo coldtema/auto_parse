b676a127e316dda1d4a9005df59ef25720110d53
coldtema <coldtema1703@gmail.com>
2025-09-10 20:52:35 +0300

    added temp traceback

--- encar_parse/parser/views.py
+++ encar_parse/parser/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render
 from .forms import CarArtikulForm
 from .models import Car, Truck, CarOption, TruckOption, OptionCategory
 import time
+import traceback
 
 def time_count(func):
     def wrapper(*args, **kwargs):
@@ -60,55 +61,57 @@ def vechile(request):
                                                                    'engine_capacity': f'{truck.engine_capacity} см²',
                                                                    'release_date': f'{str(truck.release_date)[4:]}.{str(truck.release_date)[:-2]}',
                                                                    'encar_url': truck.url,
-                                                                   'photo_list': photo_list,
-                                                                   })
+                                                                   'photo_list': photo_list,                                                         })
         if kind == 'car':
             try:
-                car = Car.objects.get(encar_id=artikul)
-            except:
-                print('Ищет по dummy')
-                car = Car.objects.get(dummy_id=artikul)
-            all_car_options = OptionCategory.objects.filter(vechile='CAR').prefetch_related('caroption_set')
-            current_car_options = list(map(lambda x: CarOption.objects.get(encar_id=x).id, eval(car.options)))
-            photo_list = []
-            sorted_codes = sorted(set(map(lambda x: int(x), eval(car.photos_codes))))
-            for i in sorted_codes:
-                if i < 10: photo_list.append(f'{car.photo_url}00{i}.jpg{photo_url_params}')
-                else: photo_list.append(f'{car.photo_url}0{i}.jpg{photo_url_params}') 
-            try:
-                diagnosis = car.diagnosis
-            except:
-                diagnosis = None
+                try:
+                    car = Car.objects.get(encar_id=artikul)
+                except:
+                    print('Ищет по dummy')
+                    car = Car.objects.get(dummy_id=artikul)
+                all_car_options = OptionCategory.objects.filter(vechile='CAR').prefetch_related('caroption_set')
+                current_car_options = list(map(lambda x: CarOption.objects.get(encar_id=x).id, eval(car.options)))
+                photo_list = []
+                sorted_codes = sorted(set(map(lambda x: int(x), eval(car.photos_codes))))
+                for i in sorted_codes:
+                    if i < 10: photo_list.append(f'{car.photo_url}00{i}.jpg{photo_url_params}')
+                    else: photo_list.append(f'{car.photo_url}0{i}.jpg{photo_url_params}') 
+                try:
+                    diagnosis = car.diagnosis
+                except:
+                    diagnosis = None
 
-            try:
-                record = car.car_record
+                try:
+                    record = car.car_record
+                except:
+                    record = None
+                accidents = None
+                if record:
+                    accidents = enumerate(record.caraccident_set.all(), 1)
+                return render(request, 'parser/vechile.html', context={'all_options_list': all_car_options,
+                                                                    'current_options_list': current_car_options,
+                                                                    'full_name': f'{car.manufacturer} {car.model} {car.version}',
+                                                                    'photo_list': photo_list,
+                                                                    'encar_id': car.encar_id,
+                                                                    'category': car.version_details,
+                                                                    'kind': 'CAR',
+                                                                    'transmission': car.transmission,
+                                                                    'mileage': f'{car.mileage} км',
+                                                                    'ru_price': f'{car.ru_price} ₽',
+                                                                    'customs_duty': f'{car.customs_duty} ₽',
+                                                                    'recycling_fee': f'{car.recycling_fee} ₽',
+                                                                    'fuel_type': car.fuel_type,
+                                                                    'model_year': car.model_year,
+                                                                    'color': car.color,
+                                                                    'engine_capacity': f'{car.engine_capacity} см²',
+                                                                    'encar_url': car.url,
+                                                                    'release_date': f'{str(car.release_date)[4:]}.{str(car.release_date)[:-2]}',
+                                                                    'diagnosis': diagnosis,
+                                                                    'record': record,
+                                                                    'accidents': accidents,
+                                                                    'final_price': f'{car.recycling_fee + car.customs_duty + car.ru_price + 95000 + 20000 + 97000} ₽',
+                                                                    })
             except:
-                record = None
-            accidents = None
-            if record:
-                accidents = enumerate(record.caraccident_set.all(), 1)
-            return render(request, 'parser/vechile.html', context={'all_options_list': all_car_options,
-                                                                   'current_options_list': current_car_options,
-                                                                   'full_name': f'{car.manufacturer} {car.model} {car.version}',
-                                                                   'photo_list': photo_list,
-                                                                   'encar_id': car.encar_id,
-                                                                   'category': car.version_details,
-                                                                   'kind': 'CAR',
-                                                                   'transmission': car.transmission,
-                                                                   'mileage': f'{car.mileage} км',
-                                                                   'ru_price': f'{car.ru_price} ₽',
-                                                                   'customs_duty': f'{car.customs_duty} ₽',
-                                                                   'recycling_fee': f'{car.recycling_fee} ₽',
-                                                                   'fuel_type': car.fuel_type,
-                                                                   'model_year': car.model_year,
-                                                                   'color': car.color,
-                                                                   'engine_capacity': f'{car.engine_capacity} см²',
-                                                                   'encar_url': car.url,
-                                                                   'release_date': f'{str(car.release_date)[4:]}.{str(car.release_date)[:-2]}',
-                                                                   'diagnosis': diagnosis,
-                                                                   'record': record,
-                                                                   'accidents': accidents,
-                                                                   'final_price': f'{car.recycling_fee + car.customs_duty + car.ru_price + 95000 + 20000 + 97000} ₽',
-                                                                   })
+                traceback.print_exc()
         return render(request, 'parser/vechile.html')
     return render(request, 'parser/vechile.html')
